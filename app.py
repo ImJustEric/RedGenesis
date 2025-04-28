ca5d1ecf1af38cab1b3f7241c11e28f2fb3ccee2
Eric Wang <60803268+ImJustEric@users.noreply.github.com>
2025-04-28 18:06:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,7 @@ from flask import redirect, render_template, session, Flask, request, jsonify
 from openai import OpenAI
 import base64
 import os
-
-# For keeping website alive every 14 minutes 
-import threading
-import requests
-import time
+import random
 
 # Configure application 
 app = Flask(__name__)
@@ -45,8 +41,7 @@ def response():
     prompt_end = f". Make sure to not include any labels and make it seem more scientific."
 
     final_prompt = prompt_intro + prompt_input + prompt_end
-
-    try:
+    try:   
         client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
         result = client.images.generate(
             model="dall-e-3",
@@ -60,24 +55,8 @@ def response():
         image_bytes = base64.b64decode(image_base64)
 
     except Exception:
-        with open('static/default_body.png', 'rb') as f:
+        random_num = random.randint(1, 13)
+        with open(f'static/copy_image_{random_num}.png', 'rb') as f:
             image_base64 = base64.b64encode(f.read()).decode('utf-8')
     
     return jsonify({'image_bytes': image_base64})
-    
-
-# Function to keep website alive 
-def self_ping():
-    while True:
-        try:
-            print("Self-pinging to stay awake...")
-            requests.get("https://redgenesis.onrender.com/")
-        except Exception as e:
-            print(f"Ping failed: {e}")
-        time.sleep(780)  # Ping every 13 minutes
-
-if __name__ == "__main__":
-    # Start self-pinging in the background 
-    threading.Thread(target=self_ping, daemon=True).start()
-    
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
